fae5717e116ec3607c84d81792303df291c7599c
Semyon <49909783+Jardaxion@users.noreply.github.com>
2022-06-10 14:13:08 +0700

    final version with auto access

--- script.js
+++ script.js
@@ -1,38 +1,34 @@
 //Function for on or off visible options
 function OnOffselect(idSelect, idOption){
-    $('#'+idSelect).toggleClass('active');
-    $('#'+idOption).toggleClass('active');
+    $(idSelect).toggleClass('active');
+    $(idOption).toggleClass('active');
 }
 
 //Function for replace text from options to headline select
 function replace(idSelect, idOption, idOptions){
-    let select = document.querySelector('#'+idSelect+' p:first-of-type')
-    let option = document.getElementById(idOption)
+    let select = document.querySelector(idSelect+' p:first-of-type')
+    let option = document.querySelector('#'+idOption)
     select.innerHTML = option.innerHTML;
     OnOffselect(idSelect, idOptions);
 }
 
 //Function fro first replace text from options to headline select
 function firstReplace(idSelect, idOption){
-    let select = document.querySelector('#'+idSelect+' p:first-of-type')
-    let option = document.getElementById(idOption)
+    let select = document.querySelector(idSelect+' p:first-of-type')
+    let option = document.querySelector(idOption)
     select.innerHTML = option.innerHTML;
 }
 
-//Applying Functions to Select
-firstReplace('nameSel', 'nameOp0') //Aplying frist replace function
-// for first meaning we take the id of the header of the custom select, for second meaning we take the id of first option
+$('.select_body').each(function(){
+    let selectBody = '#'+$(this).attr('id') //We write down Id select (It is necessary to distinguish them from each other)
+    firstReplace(selectBody+' .select', selectBody+' .options .option:first-of-type')//Aplying frist replace function
 
-$('#nameSel').click(function(){ //Set click on header custom select
-    OnOffselect('nameSel', 'nameOps') //Aplying on or off function
-    //for first meaning we take the id of the header of the custom select, for second meaning we take the id of .options
-})
+    $(selectBody + ' .select').click(function(){ //Set click on header custom select
+        OnOffselect(selectBody + ' .select', selectBody + ' .options')//Aplying on or off function
+    })
 
-$('#nameOps .option').click(function(){ //Set click on option 
-    replace('nameSel', this.getAttribute('id'), 'nameOps') //Aplying replace function
-    // for first meaning we take the id of the header of the custom select, for second meaning we take the id of third we take the id of the header of the .options
-
-    $('#nameOriginalSelect').prop("selectedIndex", this.getAttribute('id').substr(6)) 
-    //We look at which element we have selected and select the same element in the usual select
-    //In substr we write the number of characters in id .option up to numbers, for example, in nameOp we have 10 characters, so we write 10
+    $(selectBody + ' .options .option').click(function(){ //Set click on option
+        replace(selectBody + ' .select', $(this).attr('id'), selectBody + ' .options') //Aplying replace function
+        $(selectBody + ' select').prop('selectedIndex', this.getAttribute('id').replace(/[^\d]/g, '')) //We look at which element we have selected and select the same element in the usual select
+    })
 })
